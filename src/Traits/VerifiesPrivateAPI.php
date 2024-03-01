10a71af1131818818b134debf2f5a0685832ee90
Sylvester Damgaard <syda@tv2reg.dk>
2024-03-01 20:16:35 +0100

    Handle prefixes and data rewrite for assets, advanced_assets, entries, terms and users

--- src/Traits/VerifiesPrivateAPI.php
+++ src/Traits/VerifiesPrivateAPI.php
@@ -29,10 +29,29 @@ trait VerifiesPrivateAPI
                 return;
             }
 
+            // add a prefix to the data if there's only a single term
+            if ($field->type() === 'terms' && is_string($data) && count($field->config()['taxonomies']) === 1 ) {
+                $data = $field->config()['taxonomies'][0] .'::'. $data;
+            }
+
+            // add a container prefix to the assets
+            if (in_array($field->type(), ['assets', 'advanced_assets'])) {
+                if (is_string($data)) {
+                    $data = $field->config()['container'] .'::'. $data;
+                } else {
+                    $data = collect($data)->transform(function($asset) use ($field) {
+                        return $field->config()['container'] .'::'. $asset;
+                    })->toArray();
+                }
+            }
+
             $data = match ($field->type()) {
                 'bard' => is_string($data) ? $data : json_encode($data),
                 'assets' => is_string($data) ? [$data] : $data,
+                'advanced_assets' => is_string($data) ? [$data] : $data,
                 'entries' => is_string($data) ? [$data] : $data,
+                'terms' => is_string($data) ? [$data] : $data,
+                'users' => is_string($data) ? [$data] : $data,
                 default => $data
             };
 
